27739bbac16134efaeee5a2eca6de9a79ef51d3f
setyaardik <111202214555@mhs.dinus.ac.id>
2025-01-22 15:54:16 +0700

    updated 6

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,18 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import KotaSemarang from "./pages/kotasemarang";
 import KabPemalang from "./pages/kabpemalang";
 import KabPurbalingga from "./pages/kabpurbalingga";
-import KabSragen from "./pages/kabsragen"
-import KabTegal from "./pages/kabtegal"
-import KabWonosobo from "./pages/kabwonosobo"
-import KabKendal from "./pages/kabkendal"
-import KotaPekalongan from "./pages/kotapekalongan"
+import KabSragen from "./pages/kabsragen";
+import KabTegal from "./pages/kabtegal";
+import KabWonosobo from "./pages/kabwonosobo";
+import KabKendal from "./pages/kabkendal";
+import KotaPekalongan from "./pages/kotapekalongan";
+import KabBanjarnegara from "./pages/kabbanjarnegara";
+import KabBanyumas from "./pages/kabbanyumas";
+import KabBrebes from "./pages/kabbrebes";
+import KabPekalongan from "./pages/kabpekalongan";
+import KabBatang from "./pages/kabbatang";
+import KabBlora from "./pages/kabblora";
+import KabBoyolali from "./pages/kabboyolali";
 
 const App = () => (
   <Router>
@@ -20,6 +27,14 @@ const App = () => (
       <Route path="/kabwonosobo" element={<KabWonosobo />} />
       <Route path="/kabkendal" element={<KabKendal />} />
       <Route path="/kotapekalongan" element={<KotaPekalongan />} />
+      <Route path="/kabbanjarnegara" element={<KabBanjarnegara />} />
+      <Route path="/kabbanyumas" element={<KabBanyumas />} />
+      <Route path="/kabbrebes" element={<KabBrebes />} />
+      <Route path="/kabpekalongan" element={<KabPekalongan />} />
+      <Route path="/kabbatang" element={<KabBatang />} />
+      <Route path="/kabblora" element={<KabBlora />} />
+      <Route path="/kabboyolali" element={<KabBoyolali />} />
+      <Route path="/kabsemarang" element={<KabSemarang />} />
     </Routes>
   </Router>
 );
